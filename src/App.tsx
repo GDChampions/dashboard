1fff9fb3c615bf33e64b48d00a3ebc56f0b84398
TechStudent10 <76978184+TechStudent10@users.noreply.github.com>
2026-06-28 19:54:33 -0600

    the entire thing

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useRef, useState } from 'react'
+
+type Response = {
+    message: string
+    rowCount: number
+    data: any
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const input = useRef<HTMLInputElement>(null);
+    const [result, setResult] = useState<Response | null>(null);
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    return <div>
+        <center>
+            <h1>Champions Whitelist Manager</h1>
+            <p>only for super cool internal people</p>
 
-      <div className="ticks"></div>
+            <h3>File Upload</h3>
+            <input type="file" name="whitelists" ref={input} />
+            <button type="button" onClick={async (e) => {
+                if (!input.current || !input.current.files || !input.current.files[0]) return
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+                const file = input.current.files[0]
+                let formData = new FormData()
+                formData.append("file", file)
+                const result = await fetch("http://localhost:41010/upload", {
+                    method: "POST",
+                    body: formData
+                })
+                setResult(await result.json())
+            }}>Submit</button>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+            {result && <>
+                <p>Server responded with: <b>{result.message}</b></p>
+                <p>Users updated: <b>{result.rowCount}</b></p>
+            </>}
+        </center>
+    </div>
 }
 
 export default App
